a5a4f9f47efe208b5112be38a7dbe968edccd8d6
Gonzalo Martinez <gonzalo.martinez.ra@gmail.com>
2016-04-24 20:27:43 -0300

    CHANGE: Uso de ReviewAnalyzer en Index.java

--- src/luceneprueba/Index.java
+++ src/luceneprueba/Index.java
@@ -13,10 +13,9 @@ import java.nio.file.FileSystems;
 import java.util.Iterator;
 
 import org.apache.lucene.analysis.Analyzer;
-import org.apache.lucene.analysis.standard.StandardAnalyzer;
+//import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
-import org.apache.lucene.document.StringField;
 import org.apache.lucene.document.TextField;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
